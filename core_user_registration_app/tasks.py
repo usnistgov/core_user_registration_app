401e2fc678bc4e57dfe6d6772302cf336fa76f9f
pdessauw <philippe.dessauw@nist.gov>
2022-01-11 17:49:17 -0500

    fix: error in delete_user_data_structure task
    Change-Id: Ia23b106f226f3231554d4c86b088b58d6d49893a

--- core_user_registration_app/tasks.py
+++ core_user_registration_app/tasks.py
@@ -25,7 +25,8 @@ def delete_user_data_structure():
         user_data_structure_perm = UserDataStructure.get_permission()
         for data_structure_element in system_api.get_all_data_structure_elements():
             if (
-                data_structure_element.data_structure.get_object_permission()
+                data_structure_element.data_structure
+                and data_structure_element.data_structure.get_object_permission()
                 == user_data_structure_perm
             ):
                 if data_structure_element.creation_date < timezone.now() - timedelta(
