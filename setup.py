688e5aaaaeb2072a0c48649476b13b15e4d36273
Guillaume Sousa <guillaume.sousa@nist.gov>
2022-12-14 15:27:52 +0000

    chore: upgrade 2.2.0
    Change-Id: Ie2c308e96b9972597de4a3153be292fdf4bf9422

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="core_user_registration_app",
-    version="2.1.0",
+    version="2.2.0",
     description="User registration module for the core project",
     long_description=long_desc,
     author="NIST IT Lab",
